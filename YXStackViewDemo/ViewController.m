58a8b233589584a071584ed994d5d21991198211
Liubo <liubo@yixia.com>
2017-10-12 18:08:42 +0800

    Initial Commit

---
+++ YXStackViewDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YXStackViewDemo
+//
+//  Created by 刘博 on 2017/10/12.
+//  Copyright © 2017年 刘博. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ YXStackViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YXStackViewDemo
+//
+//  Created by 刘博 on 2017/10/12.
+//  Copyright © 2017年 刘博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
